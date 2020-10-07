75b2f8fa64dc26d246041fad07205415808d05ed
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-10-07 12:25:58 +0900

    first commit

---
+++ src/components/Contents.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h2>Hello HelloWorld</h2>
+    <p>{{ value }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contents",
+  props: {
+    value: String,
+  },
+};
+</script>
+
+<style scoped>
+</style>
